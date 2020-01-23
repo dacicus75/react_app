4be5f1be0c2315c4cd0649db6b2e8656569989b9
dacicus75 <dacicus75@hotmail.com>
2020-01-22 21:43:56 -0500

    reorganized file structure

--- react-client/src/components/app.jsx
+++ react-client/src/components/app.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import { browserHistory } from 'react-router';
-import HomePage from './HomePage';
-import NavBar from './HeaderComponent/NavBar';
+import HomePage from './pages/homePage.jsx';
+import NavBar from './headerComponent/NavBar.jsx';
+import Footer from './footerComponent/footer.jsx';
 class App extends Component {
   render() {
     return (
@@ -10,6 +11,7 @@ class App extends Component {
         <div>
           <NavBar />
           <Route name="home" exact path="/" component={HomePage} />
+          <Footer />
         </div>
       </Router>
     )
